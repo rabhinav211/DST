c49e5f50162951c8bf11736eb2039cb5e41ecc57
rabhinav211 <153578832+rabhinav211@users.noreply.github.com>
2024-02-15 12:23:10 +0530

    Create rotate.c

---
+++ Lab 8/rotate.c
@@ -0,0 +1,29 @@
+struct ListNode* rotateRight(struct ListNode* head, int k) {
+    if(head==NULL)
+        return head;
+    int length=1;
+    struct ListNode *temp=head,*temp2;
+
+    while(temp->next!=NULL){
+        temp=temp->next;
+        length++;
+    }
+    int rotate = length-(k%length);
+    if(rotate==0)
+        return head;
+    rotate=rotate-1;
+    temp->next=head;
+    temp=head;
+    while(rotate!=0){
+        rotate--;
+        temp=temp->next;
+    }
+    temp2=temp->next;
+    head=temp2;
+    temp->next=NULL;
+
+    return head;
+
+
+
+}
